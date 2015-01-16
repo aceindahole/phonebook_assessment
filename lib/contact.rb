e41de90163a7dd8e68976aea60fc36ef18d6ca65
Tyler Brown <elementalplug@gmail.com>
2015-01-16 11:36:29 -0800

    Add the instance method save to store contact names as well as add class methods all and clear to display and clear said contacts

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,8 +1,21 @@
 class Contact
   attr_reader(:contact_name)
+  @@contacts = []
 
   define_method(:initialize) do |attributes|
     @contact_name = attributes.fetch(:contact_name)
   end
-  
+
+  define_singleton_method(:all) do
+    @@contacts
+  end
+
+  define_method(:save) do
+    @@contacts.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@contacts = []
+  end
+
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,11 +3,36 @@ require('contact')
 require('phone')
 
 describe(Contact) do
+  before() do
+    Contact.clear()
+  end
 
   describe('#contact_name') do
     it('returns the name of the contact') do
-      test_contact = Contact.new({:contact_name => "Tyler"})
-      expect(test_contact.contact_name()).to(eq("Tyler"))
+      test_contact = Contact.new({:contact_name => "Tyler Brown"})
+      expect(test_contact.contact_name()).to(eq("Tyler Brown"))
+    end
+  end
+
+  describe('.all') do
+    it('shows the current array of added contacts') do
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves the contact name to the array of previously saved contact names') do
+      test_contact = Contact.new({:contact_name => "Tyler Brown"})
+      test_contact.save()
+      expect(Contact.all()).to(eq([test_contact]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties out all of the saved contacts') do
+      Contact.new({:contact_name => "Tyler Brown"}).save()
+      Contact.clear()
+      expect(Contact.all()).to(eq([]))
     end
   end
 

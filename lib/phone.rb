e5e88778fe17ce4b19062ef99a464a82765e4696
Tyler Brown <elementalplug@gmail.com>
2015-01-16 12:19:37 -0800

    Add the same functionality of the save all and clear methods that were in the Contact class to the Phone class and passed all specs

--- lib/phone.rb
+++ lib/phone.rb
@@ -1,8 +1,21 @@
 class Phone
   attr_reader(:phone_number)
+  @@numbers = []
 
   define_method(:initialize) do |attributes|
     @phone_number = attributes.fetch(:phone_number)
+  end
+
+  define_singleton_method(:all) do
+    @@numbers
+  end
+
+  define_method(:save) do
+    @@numbers.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@numbers = []
   end
 
 end
--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -11,5 +11,26 @@ describe(Phone) do
     end
   end
 
+  describe('.all') do
+    it('shows the current array of added phone numbers') do
+      expect(Phone.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves the phone number to the array of previously saved phone numbers') do
+      test_number = Phone.new({:phone_number => "503 577 1899"})
+      test_number.save()
+      expect(Phone.all()).to(eq([test_number]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties out all of the saved phone numbers') do
+      Phone.new({:phone_number => "503 577 1899"}).save()
+      Phone.clear()
+      expect(Contact.all()).to(eq([]))
+    end
+  end
 
 end
